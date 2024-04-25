Fix error and add bounds calculation for multi stock

src/multi_stock_bellman_value_calculation.py
@@ -24,7 +24,7 @@ def calculate_bellman_value_multi_stock(
     output_path: str,
     X: Dict[str, Array1D],
     name_solver: str = "CLP",
-) -> Dict[int, Dict[str, npt.NDArray[np.float32]]]:
+) -> tuple[Dict[int, Dict[str, npt.NDArray[np.float32]]], float, float]:
     """Function to calculate multivariate Bellman values
 
     Args:
@@ -104,14 +104,12 @@ def calculate_bellman_value_multi_stock(
                 _, _, Vu, slope, _ = solve_problem_with_multivariate_bellman_values(
                     multi_bellman_value_calculation=multi_bellman_value_calculation,
                     V=V[week + 1],
-                    scenario=scenario,
                     level_i={
                         area: multi_bellman_value_calculation.dict_reservoirs[
                             area
                         ].stock_discretization[idx[i]]
                         for i, area in enumerate(m.range_reservoir)
                     },
-                    week=week,
                     m=m,
                     take_into_account_z_and_y=True,
                 )
@@ -119,16 +117,71 @@ def calculate_bellman_value_multi_stock(
                 for area in m.range_reservoir:
                     V[week][f"slope_{area}"][idx] += slope[area] / param.len_scenario
 
-    # V_fut = interp1d(X, V[:, 0])
-    # V0 = V_fut(reservoir_management.reservoir.initial_level)
+    lower_bound = max(
+        [
+            sum(
+                [
+                    V[0][f"slope_{area}"][idx]
+                    * (
+                        multi_bellman_value_calculation.dict_reservoirs[
+                            area
+                        ].reservoir_management.reservoir.initial_level
+                        - multi_bellman_value_calculation.dict_reservoirs[
+                            area
+                        ].stock_discretization[idx[i]]
+                    )
+                    for i, area in enumerate(
+                        multi_bellman_value_calculation.dict_reservoirs.keys()
+                    )
+                ]
+            )
+            + V[0]["intercept"][idx]
+            for idx in multi_bellman_value_calculation.get_product_stock_discretization()
+        ]
+    )
 
-    # upper_bound, controls, current_itr = compute_upper_bound(
-    #     bellman_value_calculation=bellman_value_calculation,
-    #     list_models=list_models,
-    #     V=V,
-    # )
+    upper_bound = compute_upper_bound_multi_stock(
+        param=param,
+        multi_bellman_value_calculation=multi_bellman_value_calculation,
+        list_models=list_models,
+        V=V,
+    )
 
-    # gap = upper_bound + V0
-    # print(gap, upper_bound, -V0)
+    return V, lower_bound, upper_bound
 
-    return V
+
+def compute_upper_bound_multi_stock(
+    param: TimeScenarioParameter,
+    multi_bellman_value_calculation: MultiStockBellmanValueCalculation,
+    list_models: Dict[TimeScenarioIndex, AntaresProblem],
+    V: Dict[int, Dict[str, npt.NDArray[np.float32]]],
+) -> float:
+
+    cout = 0.0
+    for scenario in range(param.len_scenario):
+
+        level_i = {
+            area: multi_bellman_value_calculation.dict_reservoirs[
+                area
+            ].reservoir_management.reservoir.initial_level
+            for area in multi_bellman_value_calculation.dict_reservoirs.keys()
+        }
+
+        for week in range(param.len_week):
+            print(f"{scenario} {week}", end="\r")
+            m = list_models[TimeScenarioIndex(week, scenario)]
+
+            _, _, current_cost, _, level_i = (
+                solve_problem_with_multivariate_bellman_values(
+                    multi_bellman_value_calculation=multi_bellman_value_calculation,
+                    V=V[week + 1],
+                    level_i=level_i,
+                    m=m,
+                    take_into_account_z_and_y=(week == param.len_week - 1),
+                )
+            )
+            cout += current_cost
+
+    upper_bound = cout / param.len_scenario
+
+    return upper_bound

src/optimization.py
@@ -607,14 +607,14 @@ class AntaresProblem:
 def solve_problem_with_multivariate_bellman_values(
     multi_bellman_value_calculation: MultiStockBellmanValueCalculation,
     V: Dict[str, npt.NDArray[np.float32]],
-    scenario: int,
     level_i: Dict[str, float],
-    week: int,
     m: AntaresProblem,
     take_into_account_z_and_y: bool,
 ) -> tuple[float, int, float, Dict[str, float], Dict[str, float]]:
 
     cout = 0.0
+
+    len_reservoir = len(m.range_reservoir)
 
     for area in m.range_reservoir:
         m.stored_variables_and_constraints[area]["energy_constraint"].SetCoefficient(
@@ -627,40 +627,68 @@ def solve_problem_with_multivariate_bellman_values(
             m.stored_variables_and_constraints[area]["penalties"], 1
         )
         m.solver.Objective().SetCoefficient(
-            m.stored_variables_and_constraints[area]["final_bellman_value"], 1
+            m.stored_variables_and_constraints[area]["final_bellman_value"],
+            1 / len_reservoir,
         )
 
     additional_constraint: List = []
     iterate_stock_discretization = (
         multi_bellman_value_calculation.get_product_stock_discretization()
     )
-    len_reservoir = len(m.range_reservoir)
-    for idx in iterate_stock_discretization:
-        for area in m.range_reservoir:
 
+    for idx in iterate_stock_discretization:
+
+        for a in m.range_reservoir:
             cst = m.solver.LookupConstraint(
-                f"BellmanValue{idx}::area<all_areas>::week<{m.week}>"
+                f"BellmanValue{idx}::area<{a}>::week<{m.week}>"
             )
+
             if cst:
+
                 cst.SetCoefficient(
-                    m.stored_variables_and_constraints[area]["final_level"],
-                    float(-V[f"slope_{area}"][idx]),
+                    m.stored_variables_and_constraints[a]["final_bellman_value"],
+                    1.0,
                 )
-                cst.SetCoefficient(
-                    m.stored_variables_and_constraints[area]["final_bellman_value"],
-                    float(1 / len_reservoir),
+                cst.SetLb(
+                    float(
+                        V["intercept"][idx]
+                        - sum(
+                            [
+                                V[f"slope_{area}"][idx]
+                                * multi_bellman_value_calculation.dict_reservoirs[
+                                    area
+                                ].stock_discretization[idx[i]]
+                                for i, area in enumerate(m.range_reservoir)
+                            ]
+                        )
+                    )
                 )
-                cst.SetLb(float(V["intercept"][idx]))
+
+                for i, area in enumerate(m.range_reservoir):
+                    cst.SetCoefficient(
+                        m.stored_variables_and_constraints[area]["final_level"],
+                        float(-V[f"slope_{area}"][idx]),
+                    )
+
             else:
                 cst = m.solver.Add(
-                    m.stored_variables_and_constraints[area]["final_bellman_value"]
-                    / len_reservoir
-                    >= V[f"slope_{area}"][idx]
-                    * m.stored_variables_and_constraints[area]["final_level"]
+                    m.stored_variables_and_constraints[a]["final_bellman_value"]
+                    >= sum(
+                        [
+                            V[f"slope_{area}"][idx]
+                            * (
+                                m.stored_variables_and_constraints[area]["final_level"]
+                                - multi_bellman_value_calculation.dict_reservoirs[
+                                    area
+                                ].stock_discretization[idx[i]]
+                            )
+                            for i, area in enumerate(m.range_reservoir)
+                        ]
+                    )
                     + V["intercept"][idx],
-                    name=f"BellmanValue{idx}::area<all_areas>::week<{m.week}>",
+                    name=f"BellmanValue{idx}::area<{a}>::week<{m.week}>",
                 )
-        additional_constraint.append(cst)
+            additional_constraint.append(cst)
 
     for area in m.range_reservoir:
         cst_initial_level = m.solver.LookupConstraint(
@@ -719,7 +747,7 @@ def solve_problem_with_multivariate_bellman_values(
 
         cout += beta
         if not (take_into_account_z_and_y):
-            cout += -sum(z.values()) - sum(y.values())
+            cout += -sum(z.values()) / len_reservoir - sum(y.values())
 
     else:
         print(f"Failed at upper bound : {solve_status}")

tests/test_bellman_value_exact_two_stocks.py
@@ -3,7 +3,7 @@ from functions_iterative import (
     ReservoirManagement,
     TimeScenarioIndex,
 )
-from multi_stock_bellman_value_calculation import calculate_bellman_value_multi_stock
+from multi_stock_bellman_value_calculation import *
 from calculate_reward_and_bellman_values import (
     MultiStockManagement,
     BellmanValueCalculation,
@@ -75,6 +75,175 @@ def test_iterate_over_stock_discretization() -> None:
     ]
 
 
+def test_solve_with_bellman_multi_stock() -> None:
+
+    param = TimeScenarioParameter(len_week=5, len_scenario=1)
+
+    reservoir_1 = Reservoir("test_data/two_nodes", "area_1")
+    reservoir_management_1 = ReservoirManagement(
+        reservoir=reservoir_1,
+        penalty_bottom_rule_curve=3000,
+        penalty_upper_rule_curve=3000,
+        penalty_final_level=3000,
+        force_final_level=True,
+    )
+
+    reservoir_2 = Reservoir("test_data/two_nodes", "area_2")
+    reservoir_management_2 = ReservoirManagement(
+        reservoir=reservoir_2,
+        penalty_bottom_rule_curve=3000,
+        penalty_upper_rule_curve=3000,
+        penalty_final_level=3000,
+        force_final_level=True,
+    )
+
+    multi_stock_management = MultiStockManagement(
+        [reservoir_management_1, reservoir_management_2]
+    )
+
+    x_1 = np.linspace(0, reservoir_1.capacity, num=5)
+    x_2 = np.linspace(0, reservoir_2.capacity, num=5)
+    X = {"area_1": x_1, "area_2": x_2}
+
+    m = AntaresProblem(
+        scenario=0,
+        week=0,
+        path="test_data/two_nodes",
+        itr=1,
+    )
+    m.create_weekly_problem_itr(
+        param=param,
+        multi_stock_management=multi_stock_management,
+    )
+
+    reward: Dict[str, Dict[TimeScenarioIndex, RewardApproximation]] = {}
+    for area, reservoir_management in multi_stock_management.dict_reservoirs.items():
+        reward[area] = {}
+        for week in range(param.len_week):
+            for scenario in range(param.len_scenario):
+                r = RewardApproximation(
+                    lb_control=-reservoir_management.reservoir.max_pumping[week],
+                    ub_control=reservoir_management.reservoir.max_generating[week],
+                    ub_reward=0,
+                )
+                reward[area][TimeScenarioIndex(week, scenario)] = r
+
+    bellman_value_calculation = []
+    for area, reservoir_management in multi_stock_management.dict_reservoirs.items():
+        bellman_value_calculation.append(
+            BellmanValueCalculation(
+                param=param,
+                reward=reward[area],
+                reservoir_management=reservoir_management,
+                stock_discretization=X[area],
+            )
+        )
+    multi_bellman_value_calculation = MultiStockBellmanValueCalculation(
+        bellman_value_calculation
+    )
+
+    V = {
+        "intercept": np.array(
+            [
+                [2.7942497e09, 1.3248531e09, 1.2739775e09, 1.2459432e09, 1.2459430e09],
+                [1.3137651e09, 7.6301440e07, 3.7071564e07, 3.1171368e07, 3.1171356e07],
+                [4.1457952e08, 5.7581808e07, 3.0331620e07, 3.0331258e07, 3.0331248e07],
+                [4.0567504e08, 5.7574440e07, 3.0331604e07, 3.0331258e07, 3.0331248e07],
+                [4.0567504e08, 5.7574440e07, 3.0331604e07, 3.0331258e07, 3.0331248e07],
+            ]
+        ),
+        "slope_area_1": np.array(
+            [
+                [
+                    -8.0000000e03,
+                    -8.0000000e03,
+                    -8.0000000e03,
+                    -8.0000000e03,
+                    -8.0000000e03,
+                ],
+                [
+                    -5.0000000e03,
+                    -1.5355737e02,
+                    -1.1516584e02,
+                    -1.1516584e02,
+                    -1.1516584e02,
+                ],
+                [
+                    -5.0000000e02,
+                    -1.3635868e01,
+                    -3.0350534e-04,
+                    0.0000000e00,
+                    0.0000000e00,
+                ],
+                [0.0000000e00, 0.0000000e00, 0.0000000e00, 0.0000000e00, 0.0000000e00],
+                [0.0000000e00, 0.0000000e00, 0.0000000e00, 0.0000000e00, 0.0000000e00],
+            ]
+        ),
+        "slope_area_2": np.array(
+            [
+                [
+                    -5.81250000e03,
+                    -1.85135559e02,
+                    -7.67796326e01,
+                    -1.42892147e-03,
+                    0.00000000e00,
+                ],
+                [
+                    -5.81250000e03,
+                    -1.53558563e02,
+                    -4.37480087e01,
+                    -1.33653477e-04,
+                    0.00000000e00,
+                ],
+                [
+                    -3.37826025e03,
+                    -1.15165504e02,
+                    -1.46546517e-03,
+                    -1.33653477e-04,
+                    0.00000000e00,
+                ],
+                [
+                    -3.10778931e03,
+                    -1.15165482e02,
+                    -1.22650794e-03,
+                    -1.33653477e-04,
+                    0.00000000e00,
+                ],
+                [
+                    -3.10778931e03,
+                    -1.15165482e02,
+                    -1.22650794e-03,
+                    -1.33653477e-04,
+                    0.00000000e00,
+                ],
+            ]
+        ),
+    }
+
+    _, _, Vu, slope, xf = solve_problem_with_multivariate_bellman_values(
+        multi_bellman_value_calculation=multi_bellman_value_calculation,
+        V=V,
+        level_i={
+            area: multi_bellman_value_calculation.dict_reservoirs[
+                area
+            ].reservoir_management.reservoir.initial_level
+            for i, area in enumerate(m.range_reservoir)
+        },
+        m=m,
+        take_into_account_z_and_y=True,
+    )
+
+    assert Vu == pytest.approx(37137636.63290527)
+
+    assert slope == pytest.approx(
+        {"area_1": -73.24028513205391, "area_2": -73.23898617935389}
+    )
+
+    assert xf == pytest.approx(
+        {"area_1": 315840.91164340125, "area_2": 668042.065886599}
+    )
+
+
 def test_bellman_value_exact_calculation_multi_stock() -> None:
 
     param = TimeScenarioParameter(len_week=5, len_scenario=1)
@@ -104,21 +273,24 @@ def test_bellman_value_exact_calculation_multi_stock() -> None:
     x_1 = np.linspace(0, reservoir_1.capacity, num=5)
     x_2 = np.linspace(0, reservoir_2.capacity, num=5)
 
-    vb = calculate_bellman_value_multi_stock(
+    vb, lb, ub = calculate_bellman_value_multi_stock(
         param=param,
         multi_stock_management=all_reservoirs,
         output_path="test_data/two_nodes",
         X={"area_1": x_1, "area_2": x_2},
     )
+    assert lb == pytest.approx(419088906.63159156)
+
+    assert ub == pytest.approx(798837417.3288715)
 
     assert vb[0]["intercept"] == pytest.approx(
         np.array(
             [
-                [2.7090407e10, 2.6058617e10, 2.6046816e10, 2.6046816e10, 2.6046816e10],
-                [7.7349028e09, 6.7031137e09, 6.6913116e09, 6.6913116e09, 6.6913116e09],
-                [9.2355341e08, 5.8537856e08, 5.8481184e08, 5.8481184e08, 5.8481184e08],
-                [9.2291040e08, 5.8490163e08, 5.8481184e08, 5.8481184e08, 5.8481184e08],
-                [9.2291040e08, 5.8490163e08, 5.8481184e08, 5.8481184e08, 5.8481184e08],
+                [9.9394519e09, 5.4620790e09, 3.9168504e09, 3.8727749e09, 3.8727749e09],
+                [7.1669325e09, 2.6933955e09, 1.1481676e09, 1.1040920e09, 1.1040919e09],
+                [5.2626033e09, 1.2132852e09, 3.5723606e08, 3.0289030e08, 3.0289018e08],
+                [3.5595026e09, 7.8633613e08, 3.2175795e08, 2.8026579e08, 2.8026570e08],
+                [2.9601462e09, 7.8094682e08, 3.1681254e08, 2.8026576e08, 2.8026570e08],
             ]
         )
     )
@@ -126,27 +298,33 @@ def test_bellman_value_exact_calculation_multi_stock() -> None:
         np.array(
             [
                 [
-                    -1.0100000e05,
-                    -1.0100000e05,
-                    -1.0100000e05,
-                    -1.0100000e05,
-                    -1.0100000e05,
+                    -1.8733332e04,
+                    -1.9000000e04,
+                    -1.9000000e04,
+                    -1.9000000e04,
+                    -1.9000000e04,
                 ],
                 [
-                    -9.8000000e04,
-                    -9.8000000e04,
-                    -9.8000000e04,
-                    -9.8000000e04,
-                    -9.8000000e04,
+                    -1.0000000e04,
+                    -9.9999990e03,
+                    -9.9999990e03,
+                    -9.9999990e03,
+                    -9.9999990e03,
                 ],
                 [
-                    -1.1916082e02,
-                    -8.6371864e01,
-                    0.0000000e00,
-                    0.0000000e00,
-                    0.0000000e00,
+                    -9.8999990e03,
+                    -2.6754175e03,
+                    -2.1557803e02,
+                    -2.1557803e02,
+                    -2.1557803e02,
                 ],
-                [0.0000000e00, 0.0000000e00, 0.0000000e00, 0.0000000e00, 0.0000000e00],
+                [
+                    -4.7500000e03,
+                    -1.6168346e02,
+                    -1.3884966e02,
+                    -1.1292289e-03,
+                    -3.4511198e-05,
+                ],
                 [0.0000000e00, 0.0000000e00, 0.0000000e00, 0.0000000e00, 0.0000000e00],
             ]
         )
@@ -154,11 +332,41 @@ def test_bellman_value_exact_calculation_multi_stock() -> None:
     assert vb[0]["slope_area_2"] == pytest.approx(
         np.array(
             [
-                [-13333.335, -115.168045, 0.0, 0.0, 0.0],
-                [-13333.335, -115.168045, 0.0, 0.0, 0.0],
-                [-3000.0, -115.16554, 0.0, 0.0, 0.0],
-                [-3000.0, -115.16548, 0.0, 0.0, 0.0],
-                [-3000.0, -115.16548, 0.0, 0.0, 0.0],
+                [
+                    -1.3333334e04,
+                    -6.0000000e03,
+                    -1.5355759e02,
+                    -1.4289215e-03,
+                    0.0000000e00,
+                ],
+                [
+                    -1.2999999e04,
+                    -6.0000000e03,
+                    -1.5355759e02,
+                    -1.4289215e-03,
+                    0.0000000e00,
+                ],
+                [
+                    -1.3200002e04,
+                    -2.6754175e03,
+                    -2.1557808e02,
+                    -1.4289215e-03,
+                    0.0000000e00,
+                ],
+                [
+                    -1.3333334e04,
+                    -2.6754175e03,
+                    -1.8513576e02,
+                    -1.2687439e-03,
+                    0.0000000e00,
+                ],
+                [
+                    -1.1000000e04,
+                    -2.6754175e03,
+                    -1.5888432e02,
+                    -1.2624825e-03,
+                    0.0000000e00,
+                ],
             ]
         )
     )
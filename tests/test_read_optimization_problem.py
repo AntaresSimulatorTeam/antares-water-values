a4e4ec1d1b1d9ed6c92ba8c2cfe7ffa722664727
Juliette-Gerbaux <juliette.gerbaux_externe@rte-france.com>
2024-12-16 15:47:17 +0100

    Add test on problem with bellman values

--- tests/test_read_optimization_problem.py
+++ tests/test_read_optimization_problem.py
@@ -1,8 +1,19 @@
+import numpy as np
 import ortools.linear_solver.pywraplp as pywraplp
 import pytest
 
-from calculate_reward_and_bellman_values import MultiStockManagement
-from functions_iterative import ReservoirManagement, TimeScenarioParameter
+from calculate_reward_and_bellman_values import (
+    BellmanValueCalculation,
+    MultiStockBellmanValueCalculation,
+    MultiStockManagement,
+    RewardApproximation,
+)
+from estimation import PieceWiseLinearInterpolator, UniVariateEstimator
+from functions_iterative import (
+    ReservoirManagement,
+    TimeScenarioIndex,
+    TimeScenarioParameter,
+)
 from optimization import AntaresProblem, Basis
 from read_antares_data import Reservoir
 
@@ -115,3 +126,65 @@ def test_create_and_modify_weekly_problem_with_xpress() -> None:
         assert lamb["area"] == pytest.approx(-3000.0013996873)
     else:
         print("Ignore test, xpress not available")
+
+
+def test_create_and_modify_weekly_problem_with_bellman_values() -> None:
+    problem = AntaresProblem(scenario=0, week=0, path="test_data/one_node", itr=1)
+    param = TimeScenarioParameter(len_week=1, len_scenario=1)
+    reservoir = Reservoir("test_data/one_node", "area")
+    res_management = ReservoirManagement(
+        reservoir=reservoir,
+        penalty_bottom_rule_curve=1000,
+        penalty_upper_rule_curve=1000,
+        penalty_final_level=0,
+        force_final_level=False,
+    )
+
+    reservoir_management = MultiStockManagement([res_management])
+    problem.create_weekly_problem_itr(
+        param=param, multi_stock_management=reservoir_management
+    )
+
+    X = np.linspace(0, reservoir.capacity, num=5)
+
+    bellman_value_calculation = MultiStockBellmanValueCalculation(
+        [
+            BellmanValueCalculation(
+                param=param,
+                reward={
+                    TimeScenarioIndex(0, 0): RewardApproximation(
+                        lb_control=-reservoir.max_pumping[0],
+                        ub_control=reservoir.max_generating[0],
+                        ub_reward=0,
+                    )
+                },
+                reservoir_management=res_management,
+                stock_discretization=X,
+            )
+        ]
+    )
+
+    V = {
+        week: PieceWiseLinearInterpolator(X, np.linspace(-5e9, -3e9, num=20))
+        for week in range(2)
+    }
+
+    problem.set_constraints_initial_level_and_bellman_values(
+        reservoir.initial_level, V[1], "area"
+    )
+
+    lp = problem.solver.ExportModelAsLpFormat(False)
+
+    with open("test_data/one_node/lp_problem.txt", "r") as file:
+        assert lp == file.read()
+
+    _, _, cout, optimal_controls, _ = problem.solve_problem_with_bellman_values(
+        bellman_value_calculation,
+        UniVariateEstimator({"area": V}),
+        {"area": reservoir.initial_level},
+        True,
+        False,
+    )
+
+    assert cout == pytest.approx(5520019463.255913)
+    assert optimal_controls["area"] == pytest.approx(2072010.9999999995)

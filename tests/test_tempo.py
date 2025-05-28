9f5553f88b51fecfbae2cbe0773c7fd563c0ae65
matiasbresciano <matias.bresciano@rte-france.com>
2025-05-28 16:15:29 +0200

    bug fix

--- tests/test_tempo.py
+++ tests/test_tempo.py
@@ -13,10 +13,10 @@ bellman_values_r = BellmanValuesTempo(gain_function=gain_function_tempo_r, capac
 gain_function_tempo_wr = GainFunctionTempo(net_load=net_load, max_control=6)
 bellman_values_wr = BellmanValuesTempo(gain_function=gain_function_tempo_wr, capacity=65, nb_week=53, start_week=9,CVar=1)
 trajectories_r=TrajectoriesTempo(bv=bellman_values_r)
-trajectories_white_and_red=TrajectoriesTempo(bv=bellman_values_wr,trajectories_red=trajectories_r.trajectories)
+trajectories_white_and_red=TrajectoriesTempo(bv=bellman_values_wr,control_trajectories_red=trajectories_r.control_trajectories)
     
 # white trajectories is calculated from trajectories_white_and_red
-trajectories_white=trajectories_white_and_red.trajectories_white
+trajectories_white=trajectories_white_and_red.control_trajectories_white
 
 def test_gain_gunction_tempo() -> None:
     # Test with max_control = 5
@@ -51,13 +51,13 @@ def test_usage_values() -> None:
 
 def test_trajectories_tempo() -> None:
 
-    assert np.array_equal(trajectories_r.trajectory_for_scenario(4), np.array([0,0,1,4,4,1,1,0,0,2,0,0,0,0,2,1,0,0,2,2,2]).astype(float))
-    assert np.array_equal(trajectories_white_and_red.white_trajectory_for_scenario(4), np.array([0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 2, 2, 2, 2, 4, 3, 1, 0, 2, 2, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 1, 0, 5, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 2, 0, 0, 2, 0, 1, 2]
+    assert np.array_equal(trajectories_r.control_trajectory_for_scenario(4), np.array([0,0,1,4,4,1,1,0,0,2,0,0,0,0,2,1,0,0,2,2,2]).astype(float))
+    assert np.array_equal(trajectories_white_and_red.control_trajectory_for_scenario_white(4), np.array([0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 2, 2, 2, 2, 4, 3, 1, 0, 2, 2, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 1, 0, 5, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 2, 0, 0, 2, 0, 1, 2]
 ).astype(float))
 
-    assert np.array_equal(trajectories_r.stock_for_scenario(4), np.array([22,22,22,21,17,13,12,11,11,11,9,9,9,9,9,7,6,6,6,4,2,0]).astype(float))
-    assert np.array_equal(trajectories_white_and_red.stock_for_scenario(4), np.array([65,65,65,65,65,65,65,65,65,64,63,61,58,52,46,41,37,36,36,32,30,30,30,30,27,25,25,25,23,21,18,18,17,17,12,11,11,11,11,11,11,11,11,11,11,7,5,5,5,3,3,2,0
+    assert np.array_equal(trajectories_r.stock_trajectory_for_scenario(4), np.array([22,22,22,21,17,13,12,11,11,11,9,9,9,9,9,7,6,6,6,4,2,0]).astype(float))
+    assert np.array_equal(trajectories_white_and_red.stock_trajectory_for_scenario(4), np.array([65,65,65,65,65,65,65,65,65,64,63,61,58,52,46,41,37,36,36,32,30,30,30,30,27,25,25,25,23,21,18,18,17,17,12,11,11,11,11,11,11,11,11,11,11,7,5,5,5,3,3,2,0
 ]).astype(float))
-    assert np.array_equal(trajectories_white_and_red.white_stock_for_scenario(4), np.array([43,43,43,43,43,43,43,43,43,42,41,39,37,35,33,29,26,25,25,23,21,21,21,21,20,19,19,19,19,19,18,18,17,17,12,11,11,11,11,11,11,11,11,11,11,7,5,5,5,3,3,2,0
+    assert np.array_equal(trajectories_white_and_red.stock_trajectory_for_scenario_white(4), np.array([43,43,43,43,43,43,43,43,43,42,41,39,37,35,33,29,26,25,25,23,21,21,21,21,20,19,19,19,19,19,18,18,17,17,12,11,11,11,11,11,11,11,11,11,11,7,5,5,5,3,3,2,0
 ]).astype(float))
     

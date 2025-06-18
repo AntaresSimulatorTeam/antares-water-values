Fix overflow to 0 and change bound on generating

src/optimization.py
@@ -1,14 +1,16 @@
 import re
+from time import time
+
+import numpy as np
+import ortools.linear_solver.pywraplp as pywraplp
+from ortools.linear_solver.python import model_builder
+from scipy.interpolate import interp1d
+
 from calculate_reward_and_bellman_values import (
-    ReservoirManagement,
     BellmanValueCalculation,
+    ReservoirManagement,
 )
 from read_antares_data import TimeScenarioParameter
-import numpy as np
-from time import time
-from scipy.interpolate import interp1d
-from ortools.linear_solver.python import model_builder
-import ortools.linear_solver.pywraplp as pywraplp
 from type_definition import Array1D, Array2D, Array3D, Array4D, List
 
 
@@ -189,7 +191,7 @@ class AntaresProblem:
             hours_in_week=hours_in_week,
             name_variable=f"^HydroLevel::area<{reservoir_management.reservoir.area}>::hour<.",
         )
-        self.delete_variable(
+        self.fix_variable(
             hours_in_week=hours_in_week,
             name_variable=f"^Overflow::area<{reservoir_management.reservoir.area}>::hour<.",
         )
@@ -197,6 +199,23 @@ class AntaresProblem:
             hours_in_week=hours_in_week,
             name_constraint=f"^AreaHydroLevel::area<{reservoir_management.reservoir.area}>::hour<.",
         )
+
+        vars = model.variables()
+
+        hyd_prod_vars = [
+            var
+            for id, var in enumerate(vars)
+            if re.search(
+                f"HydProd::area<{reservoir_management.reservoir.area}>::hour<",
+                var.name(),
+            )
+        ]
+
+        for var in hyd_prod_vars:
+            var.SetUb(
+                reservoir_management.reservoir.max_generating[self.week]
+                / reservoir_management.reservoir.hours_in_week
+            )
 
         cst = model.constraints()
         binding_id = [
@@ -300,6 +319,18 @@ class AntaresProblem:
                 var[i].SetLb(-model.Infinity())
                 var[i].SetUb(model.Infinity())
                 model.Objective().SetCoefficient(var[i], 0)
+
+    def fix_variable(
+        self, hours_in_week: int, name_variable: str, value: float = 0
+    ) -> None:
+        model = self.solver
+        var = model.variables()
+        var_id = [i for i in range(len(var)) if re.search(name_variable, var[i].name())]
+        assert len(var_id) in [0, hours_in_week]
+        if len(var_id) == hours_in_week:
+            for i in var_id:
+                var[i].SetLb(value)
+                var[i].SetUb(value)
 
     def delete_constraint(self, hours_in_week: int, name_constraint: str) -> None:
         model = self.solver